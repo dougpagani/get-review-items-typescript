234baf45f672dd14e8ca72dfb3c334f8d5c51f9d
dougpagani <redstonefreedom@gmail.com>
2019-06-05 14:56:09 -0400

    add function to get aws regions dynamically

---
+++ README.md
@@ -0,0 +1,13 @@
+# Get Review Items
+For grabbing aws resources tagged for "review"
+
+
+To compile & run the javascript:
+```sh
+npm build
+```
+
+To run the javascript:
+```sh
+npm run
+```
---
+++ index.ts
@@ -0,0 +1,29 @@
+import AWS = require('aws-sdk')
+//import util = require('util')
+//AWS.config.region = 'us-east-1'
+AWS.config.update({region: 'us-east-1'})
+
+const ec2 = new AWS.EC2();
+//console.log(ec2.config.region)
+AWS.config.update({region: 'us-east-2'})
+
+var params = {};
+//const pec2 = util.promisify(ec2.describeRegions).bind(ec2)
+
+ec2.describeRegions(params, print_regions);
+
+function print_regions(err, data) {
+    if (err) console.log(err, err.stack)
+    else console.log(data.Regions.map((obj) => (obj.RegionName)))
+
+}
+
+function get_tags(err, data) {
+    console.log(data)
+//    data.forEach
+}
+
+// Send requests to each region
+
+
+
